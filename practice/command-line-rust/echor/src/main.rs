b1666562d78522fe579e7b35d441f07e88482e76
Myoungwoo Jang <codeart@gmail.com>
2022-05-24 16:26:14 +0900

    echo done

---
+++ practice/command-line-rust/echor/src/main.rs
@@ -0,0 +1,30 @@
+use clap::{Arg, Command};
+
+fn main() {
+    let matches = Command::new("echor")
+        .version("0.1.0")
+        .author("MW")
+        .about("rust echo")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .allow_invalid_utf8(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .help("Do not print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    // println!("{:#?}", matches);
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+
+    let ending = if omit_newline { "" } else { "\n" };
+    print!("{}{}", text.join(" "), ending);
+}
